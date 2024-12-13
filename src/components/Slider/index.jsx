501a3ee9f6139269e32220c5faf5446f5fe00dc3
Falekin7 <falekinldg@gmail.com>
2024-12-12 21:30:11 -0300

    slider logic arrows

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import styles from './Slider.module.css';
 import demo1 from '/img/demo1.jpg';
 import demo2 from '/img/demo2.jpg';
@@ -7,24 +8,61 @@ import leftArrow from '/left-arrow.svg'; // Importa la flecha izquierda
 import rightArrow from '/right-arrow.svg'; // Importa la flecha derecha
 
 const Slider = () => {
-  const handlePrev = () => {
-    // Lógica para mover al slide anterior
-    console.log('Previous Slide');
-  };
+  const slideShow = useRef(null);
 
   const handleNext = () => {
-    // Lógica para mover al siguiente slide
-    console.log('Next Slide');
+    // Comprobamos que el slideShow tenga elementos dentro.
+    if (slideShow.current.children.length > 0) {
+      // Obtengo el primer elemento.
+      const firstElement = slideShow.current.children[0];
+
+      //Establezco la transicion.
+      slideShow.current.style.transition = `400ms ease-out all`;
+
+      const slideSize = slideShow.current.children[0].offsetWidth;
+
+      // Muevo el slideShow
+      slideShow.current.style.transform = `translateX(-${slideSize}px)`;
+
+      const transition = () => {
+        // Restablezco la posicion del slideShow
+        slideShow.current.style.transition = 'none';
+        slideShow.current.style.transform = `translateX(0)`;
+
+        //Agarro el primer elemento y lo envio al final.
+        slideShow.current.appendChild(firstElement);
+
+        slideShow.current.removeEventListener('transitionend', transition);
+      };
+
+      //Eventlistener para cuando termina la animacion.
+      slideShow.current.addEventListener('transitionend', transition);
+    }
+  };
+
+  const handlePrev = () => {
+    if (slideShow.current.children.length > 0) {
+      // Obtengo el ultimo elemento del slideShow:
+      const index = slideShow.current.children.length - 1;
+      const lastElement = slideShow.current.children[index];
+      // Lo reubico al principio:
+      slideShow.current.insertBefore(lastElement, slideShow.current.firstChild);
+
+      slideShow.current.style.transition = 'none';
+      const slideSize = slideShow.current.children[0].offsetWidth;
+      slideShow.current.style.transform = `translateX(-${slideSize}px)`;
+
+      setTimeout(() => {
+        slideShow.current.style.transition = '400ms ease-out all';
+        slideShow.current.style.transform = `translateX(0)`;
+      }, 30);
+    }
   };
 
   return (
     <div className={styles.mainContainer}>
       <div className={styles.contenedorPrincipal}>
-        {/*  CONTENEDOR PRINCIPAL  */}
-
-        <div className={styles.contenedorSlideshow}>
-          {/* CONTENEDOR SLIDERSHOW  */}
-
+        <div className={styles.contenedorSlideshow} ref={slideShow}>
           <div className={styles.slide}>
             <a href="https://github.com/lucio-morales">
               <img src={demo1} alt="slider image" />
